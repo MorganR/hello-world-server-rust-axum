1d4f6c93ca315bb72062726481cb6a604dc1f909
Morgan Roff <roffmorgan@gmail.com>
2022-10-18 22:16:21 +0200

    Hello world with Axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::env;
+use std::net::SocketAddr;
+
+// `&'static str` becomes a `200 OK` with `content-type: text/plain; charset=utf-8`
+async fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[tokio::main]
+async fn main() {
+    let port = match env::var("PORT") {
+        Ok(p) => p.parse::<u16>(),
+        _ => Ok(8080),
+    }
+    .unwrap();
+
+    let app = Router::new().route("/hello", get(hello));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
