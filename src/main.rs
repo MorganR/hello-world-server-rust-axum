507bc2da285ba1bea66b0c85c32aa39ebc243cb8
Morgan Roff <MorganR@users.noreply.github.com>
2022-10-19 15:40:08 +0200

    Fix IP address

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn main() {
 
     let app = Router::new().route("/hello", get(hello));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
