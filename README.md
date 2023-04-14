9db27ff748a6a70785c519234957f6f146829b47
Morgan Roff <roffmorgan@gmail.com>
2023-04-14 12:04:38 +0900

    Add some comments

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hello-world-server-rust-axum
+
 A simple hello-world Go server using Rust's Axum library.
+
+This server demonstrates some basic functionality for use in a performance analysis. It must pass
+the [Hello World server integration tests](https://github.com/MorganR/hello-world-server-test).
--- src/main.rs
+++ src/main.rs
@@ -12,25 +12,28 @@ use tower_http::compression::{CompressionLayer, predicate::SizeAbove};
 use tower_http::services::ServeDir;
 
 #[derive(Debug, Deserialize)]
+/// Query parameters for /strings/hello.
 struct HelloData {
     #[serde(default)]
     name: String,
 }
 
 #[derive(Debug, Deserialize)]
-struct MathData {
+/// Query parameters for /strings/lines.
+struct LinesData {
     #[serde(default)]
     n: usize,
 }
 
-// Re-use MathData since their needs are equivalent.
-type LinesData = MathData;
+/// Query parameters for /math/power-reciprocals-alt.
+type MathData = LinesData;
 
 const DEFAULT_GREETING: &str = "Hello, world!";
 const MAX_NAME_LEN: u16 = 500;
 
 static SLOW_DURATION: Duration = Duration::from_millis(15);
 
+/// Responds with a greeting based on the "name" paremeter.
 async fn hello(data: Query<HelloData>) -> Result<String, StatusCode> {
     if data.name.is_empty() {
         return Ok(String::from(DEFAULT_GREETING));
@@ -41,11 +44,13 @@ async fn hello(data: Query<HelloData>) -> Result<String, StatusCode> {
     Ok(format!("Hello, {}!", data.name))
 }
 
+/// Responds with a greeting after a short delay.
 async fn async_hello() -> Result<String, StatusCode> {
     tokio::time::sleep(SLOW_DURATION).await;
     Ok(String::from(DEFAULT_GREETING))
 }
 
+/// Responds with an ordered list of "n" items.
 async fn lines(data: Query<LinesData>) -> Result<Html<String>, StatusCode> {
     let mut result = "<ol>\n".to_string();
     for i in 1..=(data.n) {
@@ -55,6 +60,7 @@ async fn lines(data: Query<LinesData>) -> Result<Html<String>, StatusCode> {
     Ok(Html(result))
 }
 
+/// Computes a convergent sum with "n" terms.
 async fn math_power_reciprocals_alt(data: Query<MathData>) -> Result<String, StatusCode> {
     let mut result = 0f64;
     let mut power = 0.5f64;
@@ -83,9 +89,7 @@ async fn main() {
     }
     .expect("PORT env var must be an int, or unset");
 
-    let cpus = num_cpus::get();
-    println!("Running with {} cpus", cpus);
-
+    // Define the strings routes, which all use compression.
     let strings = Router::new()
         .route("/hello", get(hello))
         .route("/async-hello", get(async_hello))
@@ -104,7 +108,7 @@ async fn main() {
         );
 
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), port);
-    println!("listening on {}", addr);
+    println!("Listening on {}", addr);
     axum::Server::bind(&addr)
         // Disable Nagle (see https://github.com/hyperium/hyper/issues/3187).
         .tcp_nodelay(true)
